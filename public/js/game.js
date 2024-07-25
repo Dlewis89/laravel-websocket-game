2ce9cdb3787a2b1128ca27011368f41e2e97ecb5
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-07-25 10:48:14 -0400

    feat: animate frame working

--- public/js/game.js
+++ public/js/game.js
@@ -5,6 +5,20 @@ const ctx = gameCanvas.getContext('2d');
 gameCanvas.clientWidth = 480;
 gameCanvas.clientHeight = 320;
 
-ctx.fillRect(100, 100, 16, 16);
-ctx.strokeStyle = 'white';
-ctx.strokeRect(100, 100, 16, 16)
+let frame = 0;
+
+function update() {
+    frame++;
+    ctx.clearRect(0, 0, 480, 320);
+    ctx.fillRect(100, 100, 16, 16);
+    ctx.strokeStyle = 'white';
+    ctx.strokeRect(100, 100, 16, 16);
+    console.log(frame);
+}
+
+function animate() {
+    update();
+    requestAnimationFrame(animate);
+}
+
+animate();
