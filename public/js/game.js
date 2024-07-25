830654cd57f31dc18f65ed47d78b7cdad91bf1c2
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-07-25 11:14:13 -0400

    feat: player moving and world boundaries check

--- public/js/game.js
+++ public/js/game.js
@@ -2,17 +2,34 @@
 const gameCanvas = document.getElementById('game-canvas');
 const ctx = gameCanvas.getContext('2d');
 
-gameCanvas.clientWidth = 480;
-gameCanvas.clientHeight = 320;
+const gameWidth = 480;
+const gameHeight = 320;
+
+gameCanvas.clientWidth = gameWidth;
+gameCanvas.clientHeight = gameHeight;
 
 let frame = 0;
 
+let playerX = 100;
+let playerY = 100;
+const playerSize = 16;
+const playerColor = 'white'
+
+function move_player() {
+
+}
+
 function update() {
     frame++;
-    ctx.clearRect(0, 0, 480, 320);
-    ctx.fillRect(100, 100, 16, 16);
-    ctx.strokeStyle = 'white';
-    ctx.strokeRect(100, 100, 16, 16);
+
+    if (playerX > 0 && playerX + playerSize < gameWidth && playerY > 0 && playerY + playerSize < gameHeight) {
+        playerX++;
+    }
+
+    ctx.clearRect(0, 0, gameWidth, gameHeight);
+    ctx.fillRect(playerX, playerY, playerSize - 1, playerSize - 1);
+    ctx.strokeStyle = playerColor;
+    ctx.strokeRect(playerX, playerY, playerSize, playerSize);
     console.log(frame);
 }
 
